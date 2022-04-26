cf4f70133d128ab1e22a187535cb2ad16d509bca
fanz1ne <igor-ignatev-02@mail.ru>
2022-04-26 18:01:06 +0300

    СДЕДАЛ НАЧАЛЬНИКАМЕНЕ

---
+++ LearningSpace/NewNodes.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace LearningSpace
+{
+    partial class NewNodes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonBackToNodes = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // buttonBackToNodes
+            // 
+            this.buttonBackToNodes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.buttonBackToNodes.Font = new System.Drawing.Font("Algerian", 27.75F);
+            this.buttonBackToNodes.ForeColor = System.Drawing.SystemColors.Info;
+            this.buttonBackToNodes.Location = new System.Drawing.Point(500, 306);
+            this.buttonBackToNodes.Name = "buttonBackToNodes";
+            this.buttonBackToNodes.Size = new System.Drawing.Size(150, 50);
+            this.buttonBackToNodes.TabIndex = 0;
+            this.buttonBackToNodes.Text = "BACK";
+            this.buttonBackToNodes.UseVisualStyleBackColor = false;
+            this.buttonBackToNodes.Click += new System.EventHandler(this.buttonBackToNodes_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(123, 91);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(205, 43);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(500, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 51);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(167, 231);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(71, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Ты ГЕЙ?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // NewNodes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(662, 368);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonBackToNodes);
+            this.Name = "NewNodes";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NewNotes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonBackToNodes;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
